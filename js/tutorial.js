443914a49d5f8eed601757ab9777743be44798a1
fengicheng <luoyan0893@foxmail.com>
2026-03-14 11:58:27 +0800

    字色改变

--- js/tutorial.js
+++ js/tutorial.js
@@ -37,17 +37,17 @@ export class Tutorial {
         if (this.timer < 5) {
             // Show crosshair hint
             ctx.globalAlpha = 0.5 + 0.3 * Math.sin(this.timer * 3);
-            ctx.fillStyle = '#FFF';
+            ctx.fillStyle = '#ff0101';
             ctx.font = '16px Arial';
             ctx.fillText('移动鼠标瞄准，点击射击', W / 2, H / 2 - 80);
         } else if (this.timer < 12) {
             ctx.globalAlpha = Math.max(0, 1 - (this.timer - 5) / 3);
-            ctx.fillStyle = '#FF8';
+            ctx.fillStyle = 'rgb(243, 7, 7)';
             ctx.font = '14px Arial';
             ctx.fillText('屏幕即将变暗，注意雷达', W / 2, H / 2 - 80);
         } else if (this.timer < 22) {
             ctx.globalAlpha = 0.4 + 0.3 * Math.sin(this.timer * 2);
-            ctx.fillStyle = '#8F8';
+            ctx.fillStyle = 'rgb(250, 43, 6)';
             ctx.font = '14px Arial';
             ctx.fillText('击杀敌人会照亮周围区域', W / 2, H / 2 - 100);
         }
